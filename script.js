cff2869eecea092cf9be8ddd6a673a8ab14d7de5
ruyot <tabeeb700@gmail.com>
2026-01-14 18:03:32 -0500

    anime.js

---
+++ script.js
@@ -0,0 +1,13 @@
+// Shopifolk - Animation Script
+// Using anime.js v4.0
+
+// Color palette (matching CSS)
+const COLORS = {
+  nodeGreen: '#95BF47',
+  bgCream: '#F5F0EB',
+  textPrimary: '#1A1A1A',
+  textSecondary: '#4A4A4A'
+};
+
+// Animation will be built here
+console.log('Shopifolk loaded - anime.js ready');
